692c87a555dce175e0c15d9db0dbb5b4deec2a58
avinayk <avinaykumar.web@gmail.com>
2025-08-21 13:26:18 +0530

    amou

---
+++ build/static/js/3156.c9975fe8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkamourette=self.webpackChunkamourette||[]).push([[3156],{3156:(t,e,a)=>{a.d(e,{BNo:()=>r});var c=a(3441);function r(t){return(0,c.k5)({tag:"svg",attr:{viewBox:"0 0 448 512"},child:[{tag:"path",attr:{d:"M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=3156.c9975fe8.chunk.js.map
