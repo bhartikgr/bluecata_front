70425a7cb7fb5972f6d1be2e7db777639d17f61e
avinayk <avinaykumar.web@gmail.com>
2025-09-22 17:25:56 +0530

    roster

---
+++ back/routes/admin/dashboard.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const DashboardController = require("../../controllers/admin/DashboardController");
+
+// Define the POST /login route
+router.post("/getusers", DashboardController.getusers);
+router.post("/getinvoices", DashboardController.getinvoices);
+router.post("/getsponserpayments", DashboardController.getsponpays);
+router.post("/gethostpayments", DashboardController.gethostpayments);
+router.post("/pay-to-host", DashboardController.paytohost);
+router.post("/create-paypal-order", DashboardController.createpaypalorder);
+router.post("/capture-paypal-order", DashboardController.capturePaypalOrder);
+ 
+module.exports = router;
