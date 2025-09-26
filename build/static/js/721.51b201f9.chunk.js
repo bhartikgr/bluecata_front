a991a934bd6fdd0b789eb451884bdaa0c0d300d0
avinayk <avinaykumar.web@gmail.com>
2025-09-26 11:21:18 +0530

    capavate

---
+++ build/static/js/721.51b201f9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkkeiretsuproject=self.webpackChunkkeiretsuproject||[]).push([[721],{6720:(t,h,e)=>{e.d(h,{ZKV:()=>c});var a=e(13441);function c(t){return(0,a.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"},child:[]},{tag:"path",attr:{d:"M15 16h4v2h-4zm0-8h7v2h-7zm0 4h6v2h-6zM3 18c0 1.1.9 2 2 2h6c1.1 0 2-.9 2-2V8H3v10zM14 5h-3l-1-1H6L5 5H2v2h12z"},child:[]}]})(t)}},25015:()=>{},83656:()=>{}}]);
+//# sourceMappingURL=721.51b201f9.chunk.js.map
