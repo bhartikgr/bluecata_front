0728d60a0d2fd8dfdc857c2ac45b503128a8d796
avinayk <avinaykumar.web@gmail.com>
2025-09-26 14:28:36 +0530

    file

--- build/static/js/3156.006486b5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkkeiretsuproject=self.webpackChunkkeiretsuproject||[]).push([[3156],{23156:(t,e,c)=>{c.d(e,{OiG:()=>r});var s=c(13441);function r(t){return(0,s.k5)({tag:"svg",attr:{viewBox:"0 0 448 512"},child:[{tag:"path",attr:{d:"M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z"},child:[]}]})(t)}}}]);
-//# sourceMappingURL=3156.006486b5.chunk.js.map
