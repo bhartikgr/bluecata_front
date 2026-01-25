e415145a2e3f6e85877d4bb8976eb3acc8b3e043
avinayk <avinaykumar.web@gmail.com>
2026-01-25 15:18:43 +0530

    yy

--- controllers/admin/zoomController.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require("bcryptjs");
-const moment = require("moment-timezone");
-const db = require("../../db");
-const nodemailer = require("nodemailer");
-
-require("dotenv").config();
