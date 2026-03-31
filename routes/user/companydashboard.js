6e9dd6e0abee080d57e83a8da0b8567bbd88edb9
avinayk <avinaykumar.web@gmail.com>
2026-03-31 15:22:15 +0530

    build
    build

---
+++ routes/user/companydashboard.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const companydashboardController = require("../../controllers/user/companydashboardController");
+const uploadDocsMiddleware = require("../../middlewares/uploadDocsMiddleware");
+
+// Define the POST /login route
+router.post("/getroundChart", companydashboardController.getroundChart);
+router.post(
+  "/getInvitedInvestor",
+  companydashboardController.getInvitedInvestor,
+);
+
+module.exports = router;
