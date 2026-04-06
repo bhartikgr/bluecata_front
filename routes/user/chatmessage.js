4f938efa4a5a5965727271910a4c34752168b2ea
avinayk <avinaykumar.web@gmail.com>
2026-04-06 17:02:29 +0530

    build
    build

---
+++ routes/user/chatmessage.js
@@ -0,0 +1,25 @@
+// routes/user/chatmessage.js
+const express = require("express");
+const router = express.Router();
+const chatmessageController = require("../../controllers/user/chatmessageController");
+const uploadDocsMiddleware = require("../../middlewares/uploadDocsMiddleware");
+
+// Chat Routes
+router.post(
+  "/get_or_create_conversation",
+  chatmessageController.getOrCreateConversation,
+);
+router.post(
+  "/get-user-conversations",
+  chatmessageController.getUserConversations,
+);
+router.post("/get-messages", chatmessageController.getMessages);
+router.post("/send-message", chatmessageController.sendMessage);
+router.post("/delete-message", chatmessageController.deleteMessage);
+router.post(
+  "/upload-file",
+  uploadDocsMiddleware.single("file"),
+  chatmessageController.uploadFile,
+);
+
+module.exports = router;
