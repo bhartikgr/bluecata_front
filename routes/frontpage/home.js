7820ae4c65a74ffd644c05cf98e351237b027386
avinayk <avinaykumar.web@gmail.com>
2026-04-16 16:28:12 +0530

    builds
    builds

--- routes/frontpage/home.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const HomeController = require("../../controllers/frontpage/HomeController");
-
-// Define the POST /login route
-router.post("/saveContact", HomeController.saveContact);
-
-module.exports = router;
