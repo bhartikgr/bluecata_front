73cda47b92d4b83a6771bb7cd9c56a660b107a04
avinayk <avinaykumar.web@gmail.com>
2026-04-20 17:17:27 +0530

    build
    build

---
+++ routes/user/investorround.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const investorroundController = require("../../controllers/user/investorRoundController");
+const uploadDocsMiddleware = require("../../middlewares/uploadDocsMiddleware");
+
+// Define the POST /login route
+router.post("/getcompanyDetails", investorroundController.getcompanyDetails);
+router.post(
+  "/getcompanyRoundSeperateDetail",
+  investorroundController.getcompanyRoundSeperateDetail,
+);
+router.post(
+  "/getRoundCapTableSingleRecord",
+  investorroundController.getRoundCapTableSingleRecord,
+);
+router.post("/archiveInvitation", investorroundController.archiveInvitation);
+router.post(
+  "/unarchiveInvitation",
+  investorroundController.unarchiveInvitation,
+);
+module.exports = router;
