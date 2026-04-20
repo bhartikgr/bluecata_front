1f476a517879c86d7fb9c2fef02cc53a3c26abc6
avinayk <avinaykumar.web@gmail.com>
2026-04-20 15:31:37 +0530

    build
    build

--- routes/frontpage/home.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const HomeController = require("../../controllers/frontpage/HomeController");
-
-// Define the POST /login route
-router.post("/saveContact", HomeController.saveContact);
-router.post("/getallcountrySymbolList", HomeController.getallcountrySymbolList);
-router.post("/saveAcademypopup", HomeController.saveAcademypopup);
-module.exports = router;
