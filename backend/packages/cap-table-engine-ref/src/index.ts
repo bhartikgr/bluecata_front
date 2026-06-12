d377dd98bd656a31eecfc9676ac0c7e4cc42fe41
avinayk <avinaykumar.web@gmail.com>
2026-06-12 10:25:32 +0530

    build
    build

--- backend/packages/cap-table-engine-ref/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./refCapTable.js";
-export * from "./refMath.js";
-export {
-  fxFromString, fxToString, fxAdd, fxSub, fxMul, fxDiv,
-  fxFloorToShares, fxFromBigInt, FX_ZERO, FX_ONE, SCALE, SCALE_DIGITS,
-} from "./fixed.js";
